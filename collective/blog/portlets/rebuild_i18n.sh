ce4ef399ba6ba500f1eb8e1c29f057b168b9974b
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-06-13 12:57:56 +0200

    rename po files to remove the language code

--- collective/blog/portlets/rebuild_i18n.sh
+++ collective/blog/portlets/rebuild_i18n.sh
@@ -6,5 +6,5 @@ I18NDOMAIN=$PRODUCTNAME
 i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with the .po files
-i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}-*.po
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
 
