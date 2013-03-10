eaa5e143559a60295271dff7acbee0fdddc3f642
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2013-03-09 21:08:52 -0430

    Added support for extract plone domain strings

--- collective/blog/portlets/rebuild_i18n.sh
+++ collective/blog/portlets/rebuild_i18n.sh
@@ -8,3 +8,29 @@ i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTN
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
 
+# Synchronise the templates and scripts with the .pot file for plone domain.
+i18ndude rebuild-pot --pot ./locales/plone.pot \
+    --create plone \
+    ./profiles/default
+
+# Synchronise the Plone's pot file (Used for the workflows)
+for po in ./locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot ./locales/plone.pot $po
+done
+
+WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or"
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir"
+
+rm ./rebuild_i18n.log
+
+touch ./rebuild_i18n.log
+
+find ../ -name "*pt" | xargs i18ndude find-untranslated > rebuild_i18n.log
